5c20316286d6b0cabafa35f9e1d534a4de8ab58f
siagupta-03 <112824643+siagupta-03@users.noreply.github.com>
2024-07-04 00:22:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        //code here.
+        int carry= helper(head);
+        if(carry==1){
+            Node newNode= new Node(1);
+            newNode.next=head;
+            
+            return newNode;
+        }
+         return head;
+       
+        
+    }
+    public static int helper(Node temp){
+        if(temp==null)
+            return 1;
+            
+            int carry= helper(temp.next);
+            temp.data=temp.data+carry;
+            if(temp.data<10){
+                return 0;
+            }
+            temp.data=0;
+            
+            return 1;
+        }
+      
+    }
+
